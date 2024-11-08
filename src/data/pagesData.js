583dfa1ff6e706361d24b19d645f475418f3edd5
arekasek <arekas2914@wp.pl>
2024-11-08 15:42:12 +0100

    new sites

--- src/data/pagesData.js
+++ src/data/pagesData.js
@@ -276,4 +276,348 @@ export const pagesData = {
       },
     ],
   },
+  ogrodzenia: {
+    slug: "ogrodzenia",
+    metaTitle: "Woj-Płot | Ogrodzenia Artystyczne Lębork",
+    metaDescription:
+      "Oferujemy szeroką gamę ogrodzeń, wykonane są z wyjątkową dbałością o każdy szczegół, co zapewnia nie tylko atrakcyjny wygląd, ale także bezpieczeństwo i trwałość na długie lata.",
+    metaImage: "/media/ogrodzenie-tło.avif",
+    layout: [
+      {
+        blockType: "Show",
+        heading: "Ogrodzenia",
+        text: "Nasze ogrodzenia wykonane są z wyjątkową dbałością o każdy szczegół, co zapewnia nie tylko atrakcyjny wygląd, ale także bezpieczeństwo i trwałość na długie lata. Realizujemy zarówno nowoczesne, minimalistyczne projekty, jak i klasyczne, ozdobne wzory, w tym ogrodzenia kute o artystycznym charakterze. Każdy projekt dopasowujemy do indywidualnych potrzeb klienta, oferując rozwiązania idealne zarówno dla posesji prywatnych, jak i przestrzeni publicznych. Proponujemy szeroką gamę stylów i wykończeń, aby sprostać oczekiwaniom nawet najbardziej wymagających klientów. Zapewniamy również profesjonalny montaż, gwarantujący solidność i stabilność ogrodzenia na wiele lat.",
+        image: {
+          url: "/media/ogrodzenie-tło.avif",
+          alt: "Ogrodzenia Background",
+        },
+        images: [
+          {
+            id: 1,
+            image: { url: "/media/ogrodzenia/1.avif", alt: "Ogrodzenie 1" },
+          },
+          {
+            id: 2,
+            image: { url: "/media/ogrodzenia/2.avif", alt: "Ogrodzenie 2" },
+          },
+          {
+            id: 3,
+            image: { url: "/media/ogrodzenia/3.avif", alt: "Ogrodzenie 3" },
+          },
+          {
+            id: 4,
+            image: { url: "/media/ogrodzenia/4.avif", alt: "Ogrodzenie 4" },
+          },
+          {
+            id: 5,
+            image: { url: "/media/ogrodzenia/5.avif", alt: "Ogrodzenie 5" },
+          },
+          {
+            id: 6,
+            image: { url: "/media/ogrodzenia/6.avif", alt: "Ogrodzenie 6" },
+          },
+          {
+            id: 7,
+            image: { url: "/media/ogrodzenia/7.avif", alt: "Ogrodzenie 7" },
+          },
+          {
+            id: 8,
+            image: { url: "/media/ogrodzenia/8.avif", alt: "Ogrodzenie 8" },
+          },
+          {
+            id: 9,
+            image: { url: "/media/ogrodzenia/9.avif", alt: "Ogrodzenie 9" },
+          },
+          {
+            id: 10,
+            image: { url: "/media/ogrodzenia/10.avif", alt: "Ogrodzenie 10" },
+          },
+          {
+            id: 11,
+            image: { url: "/media/ogrodzenia/11.avif", alt: "Ogrodzenie 11" },
+          },
+          {
+            id: 12,
+            image: { url: "/media/ogrodzenia/12.avif", alt: "Ogrodzenie 12" },
+          },
+          {
+            id: 13,
+            image: { url: "/media/ogrodzenia/13.avif", alt: "Ogrodzenie 13" },
+          },
+          {
+            id: 14,
+            image: { url: "/media/ogrodzenia/14.avif", alt: "Ogrodzenie 14" },
+          },
+          {
+            id: 15,
+            image: { url: "/media/ogrodzenia/15.avif", alt: "Ogrodzenie 15" },
+          },
+          {
+            id: 16,
+            image: { url: "/media/ogrodzenia/16.avif", alt: "Ogrodzenie 16" },
+          },
+          {
+            id: 17,
+            image: { url: "/media/ogrodzenia/17.avif", alt: "Ogrodzenie 17" },
+          },
+          {
+            id: 18,
+            image: { url: "/media/ogrodzenia/18.avif", alt: "Ogrodzenie 18" },
+          },
+        ],
+      },
+    ],
+  },
+  furtki: {
+    slug: "furtki",
+    metaTitle: "Woj-Płot | Furtki Artystyczne Lębork",
+    metaDescription:
+      "Oferujemy szeroką gamę furtek z wyjątkową dbałością o każdy szczegół, co gwarantuje nie tylko elegancki wygląd, ale również trwałość i bezpieczeństwo.",
+    metaImage: "/media/furtka-tło.avif",
+    layout: [
+      {
+        blockType: "Show",
+        heading: "Furtki",
+        text: "Nasze furtki są tworzone z wyjątkową dbałością o każdy szczegół, co gwarantuje nie tylko elegancki wygląd, ale również trwałość i bezpieczeństwo. Oferujemy zarówno nowoczesne, minimalistyczne projekty, jak i klasyczne, dekoracyjne wzory, w tym furtki kute o artystycznym charakterze. Każdą furtkę dopasowujemy do indywidualnych potrzeb klienta, aby idealnie komponowała się z ogrodzeniem i otoczeniem. Dostępny jest szeroki wybór stylów i wykończeń, co pozwala nam sprostać oczekiwaniom nawet najbardziej wymagających klientów. Zapewniamy również fachowy montaż, który stanowi gwarancję solidności i długowieczności furtek na lata.",
+        image: {
+          url: "/media/furtka-tło.avif",
+          alt: "furtka Background",
+        },
+        images: [
+          {
+            id: 1,
+            image: { url: "/media/furtki/1.avif", alt: "furtka 1" },
+          },
+          {
+            id: 2,
+            image: { url: "/media/furtki/2.avif", alt: "furtka 2" },
+          },
+          {
+            id: 3,
+            image: { url: "/media/furtki/3.avif", alt: "furtka 3" },
+          },
+          {
+            id: 4,
+            image: { url: "/media/furtki/4.avif", alt: "furtka 4" },
+          },
+          {
+            id: 5,
+            image: { url: "/media/furtki/5.avif", alt: "furtka 5" },
+          },
+          {
+            id: 6,
+            image: { url: "/media/furtki/6.avif", alt: "furtka 6" },
+          },
+          {
+            id: 7,
+            image: { url: "/media/furtki/7.avif", alt: "furtka 7" },
+          },
+          {
+            id: 8,
+            image: { url: "/media/furtki/8.avif", alt: "furtka 8" },
+          },
+          {
+            id: 9,
+            image: { url: "/media/furtki/9.avif", alt: "furtka 9" },
+          },
+          {
+            id: 10,
+            image: { url: "/media/furtki/10.avif", alt: "furtka 10" },
+          },
+          {
+            id: 11,
+            image: { url: "/media/furtki/11.avif", alt: "furtka 11" },
+          },
+          {
+            id: 12,
+            image: { url: "/media/furtki/12.avif", alt: "furtka 12" },
+          },
+          {
+            id: 13,
+            image: { url: "/media/furtki/13.avif", alt: "furtka 13" },
+          },
+          {
+            id: 14,
+            image: { url: "/media/furtki/14.avif", alt: "furtka 14" },
+          },
+          {
+            id: 15,
+            image: { url: "/media/furtki/15.avif", alt: "furtka 15" },
+          },
+          {
+            id: 16,
+            image: { url: "/media/furtki/16.avif", alt: "furtka 16" },
+          },
+        ],
+      },
+    ],
+  },
+  konstrukcje: {
+    slug: "konstrukcje",
+    metaTitle: "Woj-Płot | Konstrukcje Metalowe Lębork",
+    metaDescription:
+      "Nasze konstrukcje metalowe, takie jak wiaty, przęsła i inne elementy, wykonujemy z najwyższą starannością, dbając o solidność, trwałość oraz estetykę.",
+    metaImage: "/media/konstrukcje-tło.avif",
+    layout: [
+      {
+        blockType: "Show",
+        heading: "Konstrukcje",
+        text: "Nasze konstrukcje metalowe, takie jak wiaty, przęsła i inne elementy, wykonujemy z najwyższą starannością, dbając o solidność, trwałość oraz estetykę. Realizujemy zarówno nowoczesne, minimalistyczne projekty, jak i bardziej tradycyjne rozwiązania, dostosowane do specyficznych potrzeb klienta. Każda konstrukcja jest projektowana z myślą o jej wytrzymałości i funkcjonalności, co sprawia, że doskonale sprawdza się w różnych warunkach atmosferycznych. Oferujemy szeroki wybór stylów, wykończeń oraz rozmiarów, by spełnić wymagania nawet najbardziej wymagających klientów. Zapewniamy również profesjonalny montaż, który gwarantuje stabilność i długowieczność naszych konstrukcji na lata.",
+        image: {
+          url: "/media/konstrukcja-tło.avif",
+          alt: "konstrukcje Background",
+        },
+        images: [
+          {
+            id: 1,
+            image: { url: "/media/konstrukcje/1.avif", alt: "konstrukcja 1" },
+          },
+          {
+            id: 2,
+            image: { url: "/media/konstrukcje/2.avif", alt: "konstrukcja 2" },
+          },
+          {
+            id: 3,
+            image: { url: "/media/konstrukcje/3.avif", alt: "konstrukcja 3" },
+          },
+          {
+            id: 4,
+            image: { url: "/media/konstrukcje/4.avif", alt: "konstrukcja 4" },
+          },
+          {
+            id: 5,
+            image: { url: "/media/konstrukcje/5.avif", alt: "konstrukcja 5" },
+          },
+          {
+            id: 6,
+            image: { url: "/media/konstrukcje/6.avif", alt: "konstrukcja 6" },
+          },
+          {
+            id: 7,
+            image: { url: "/media/konstrukcje/7.avif", alt: "konstrukcja 7" },
+          },
+          {
+            id: 8,
+            image: { url: "/media/konstrukcje/8.avif", alt: "konstrukcja 8" },
+          },
+          {
+            id: 9,
+            image: { url: "/media/konstrukcje/9.avif", alt: "konstrukcja 9" },
+          },
+          {
+            id: 10,
+            image: { url: "/media/konstrukcje/10.avif", alt: "konstrukcja 10" },
+          },
+          {
+            id: 11,
+            image: { url: "/media/konstrukcje/11.avif", alt: "konstrukcja 11" },
+          },
+          {
+            id: 12,
+            image: { url: "/media/konstrukcje/12.avif", alt: "konstrukcja 12" },
+          },
+          {
+            id: 13,
+            image: { url: "/media/konstrukcje/13.avif", alt: "konstrukcja 13" },
+          },
+          {
+            id: 14,
+            image: { url: "/media/konstrukcje/14.avif", alt: "konstrukcja 14" },
+          },
+          {
+            id: 15,
+            image: { url: "/media/konstrukcje/15.avif", alt: "konstrukcja 15" },
+          },
+          {
+            id: 16,
+            image: { url: "/media/konstrukcje/16.avif", alt: "konstrukcja 16" },
+          },
+        ],
+      },
+    ],
+  },
+  meble: {
+    slug: "meble",
+    metaTitle: "Woj-Płot | Meble Kute Lębork",
+    metaDescription:
+      "Nasze meble kute to połączenie wytrzymałości i elegancji, tworzone z dbałością o każdy detal, aby zapewnić wyjątkowy styl i trwałość na lata.",
+    metaImage: "/media/meble-tło.avif",
+    layout: [
+      {
+        blockType: "Show",
+        heading: "Meble Kute",
+        text: "Nasze meble kute to połączenie wytrzymałości, elegancji oraz wyjątkowego rzemiosła. Wykonujemy zarówno nowoczesne, minimalistyczne projekty, jak i klasyczne, dekoracyjne wzory, które idealnie komponują się w różnorodnych wnętrzach oraz przestrzeniach zewnętrznych. Każdy mebel jest projektowany z myślą o funkcjonalności i estetyce, oferując niepowtarzalny styl i trwałość. Dzięki szerokiemu wyborowi stylów i wykończeń nasze meble spełniają oczekiwania nawet najbardziej wymagających klientów. Zapewniamy również fachowy montaż, gwarantujący stabilność i solidność na długie lata.",
+        image: {
+          url: "/media/meble-tło.avif",
+          alt: "meble Background",
+        },
+        images: [
+          {
+            id: 1,
+            image: { url: "/media/meble/1.avif", alt: "mebel 1" },
+          },
+          {
+            id: 2,
+            image: { url: "/media/meble/2.avif", alt: "mebel 2" },
+          },
+          {
+            id: 3,
+            image: { url: "/media/meble/3.avif", alt: "mebel 3" },
+          },
+          {
+            id: 4,
+            image: { url: "/media/meble/4.avif", alt: "mebel 4" },
+          },
+          {
+            id: 5,
+            image: { url: "/media/meble/5.avif", alt: "mebel 5" },
+          },
+          {
+            id: 6,
+            image: { url: "/media/meble/6.avif", alt: "mebel 6" },
+          },
+          {
+            id: 7,
+            image: { url: "/media/meble/7.avif", alt: "mebel 7" },
+          },
+          {
+            id: 8,
+            image: { url: "/media/meble/8.avif", alt: "mebel 8" },
+          },
+          {
+            id: 9,
+            image: { url: "/media/meble/9.avif", alt: "mebel 9" },
+          },
+          {
+            id: 10,
+            image: { url: "/media/meble/10.avif", alt: "mebel 10" },
+          },
+          {
+            id: 11,
+            image: { url: "/media/meble/11.avif", alt: "mebel 11" },
+          },
+          {
+            id: 12,
+            image: { url: "/media/meble/12.avif", alt: "mebel 12" },
+          },
+          {
+            id: 13,
+            image: { url: "/media/meble/13.avif", alt: "mebel 13" },
+          },
+          {
+            id: 14,
+            image: { url: "/media/meble/14.avif", alt: "mebel 14" },
+          },
+          {
+            id: 15,
+            image: { url: "/media/meble/15.avif", alt: "mebel 15" },
+          },
+          {
+            id: 16,
+            image: { url: "/media/meble/16.avif", alt: "mebel 16" },
+          },
+        ],
+      },
+    ],
+  },
 };
