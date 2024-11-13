b02d358aa1e56150c12316965f9e46b858cb056a
arekasek <arekas2914@wp.pl>
2024-11-13 19:16:29 +0100

    redirects

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,45 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/new-balustrady",
+        destination: "/balustrady",
+        permanent: true,
+      },
+      {
+        source: "/new-bramy",
+        destination: "/bramy",
+        permanent: true,
+      },
+      {
+        source: "/new-ogrodzenia",
+        destination: "/ogrodzenia",
+        permanent: true,
+      },
+      {
+        source: "/new-furtki",
+        destination: "/furtki",
+        permanent: true,
+      },
+      {
+        source: "/new-konstrukcje",
+        destination: "/konstrukcje",
+        permanent: true,
+      },
+      {
+        source: "/new-meble",
+        destination: "/meble",
+        permanent: true,
+      },
+      {
+        source: "/home",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -58,7 +58,6 @@ const Page = ({ page }) => {
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:locale" content="pl_PL" />
-        <link rel="sitemap" type="application/xml" href="/sitemap.xml" />
       </Head>
 
       <RenderBlocks layout={page.layout} />
