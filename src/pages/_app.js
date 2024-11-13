7c16d4ccd6c93dc3de79ca2c512c8277c5f97a66
arekasek <arekas2914@wp.pl>
2024-11-13 17:46:52 +0100

    vercel logs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,8 @@ import Footer from "@/blocks/global/Footer";
 import Sidebar from "@/blocks/global/Sidebar";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_BACKEND_URL;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.patch["Content-Type"] = "application/json";
@@ -24,6 +26,8 @@ export default function App({ Component, pageProps }) {
       <Contact />
       <Sidebar></Sidebar>
       <Footer />
+      <Analytics />
+      <SpeedInsights />
     </>
   );
 }
